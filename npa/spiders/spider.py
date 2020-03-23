cf5dd0cc120b84b8b99d7df2356c5e06fb04fe1f
pattaradanais <dpatt_s@hotmail.com>
2020-03-24 06:02:38 +0700

    setting chromedriver

--- npa/spiders/spider.py
+++ npa/spiders/spider.py
@@ -33,7 +33,15 @@ class SpiderSpider(scrapy.Spider):
         self.chrome_options.add_argument("--headless")
         # self.driver = webdriver.Chrome(executable_path="../chromedriver", chrome_options=self.chrome_options)
         # self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self.chrome_options)
-        self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver') 
+         
+# demo.pyimport scrapyfrom selenium import webdriver
+# options = webdriver.ChromeOptions()        
+# options.add_argument("--disable-extensions")        
+# options.add_argument("--headless")        
+# options.add_argument("--disable-gpu")       
+# options.add_argument("--no-sandbox")        
+# self.driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')       
 
 
     def start_requests(self):
