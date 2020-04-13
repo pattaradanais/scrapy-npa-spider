ad0a9868078e753374ba4868fb83f16d923b5656
pattaradanais <dpatt_s@hotmail.com>
2020-04-14 05:07:09 +0700

    debug

--- build/lib/npa/pipelines.py
+++ build/lib/npa/pipelines.py
@@ -13,7 +13,7 @@ class NpaPipeline(object):
         # client = pymongo.MongoClient("mongodb+srv://npaDB:npaadmin@cluster0-ipibu.gcp.mongodb.net/npaWebAppDB?retryWrites=true&w=majority")
         client = pymongo.MongoClient("mongodb://npaDB:npaadmin@cluster0-shard-00-00-ipibu.gcp.mongodb.net:27017,cluster0-shard-00-01-ipibu.gcp.mongodb.net:27017,cluster0-shard-00-02-ipibu.gcp.mongodb.net:27017/npaWebAppDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
         db = client.get_default_database()
-        self.collection = db['raw_data']
+        self.collection = db['properties']
         # pass
 
     def process_item(self, item, spider):
