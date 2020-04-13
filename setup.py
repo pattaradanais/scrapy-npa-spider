e88fa2415dae764219f83e0f3136a1709c25feab
pattaradanais <dpatt_s@hotmail.com>
2020-04-14 03:54:22 +0700

    setup,py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version      = '1.0',
     packages     = find_packages(),
     package_data={
-        'myproject': ['assets/*.json']
+        'project': ['assets/*.json']
     },
     entry_points = {'scrapy': ['settings = npa.settings']},
 )
